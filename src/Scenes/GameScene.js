4d0d0e7560418a958f54fe9f1566d50d73aa9975
BethFromAbove <bethcott@hotmail.co.uk>
2021-04-19 21:20:36 +0100

    Added beautiful snake hair

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -5,19 +5,29 @@ var player;
 var stars;
 var bombs;
 var platforms;
+var hairGroup;
 var cursors;
 var score = 0;
 var scoreText;
 
+var hairSection1 = new Array(); //array of sprites that make the hair sections
+var hairSection2 = new Array();
+var hairSection3 = new Array();
+var hairPath1 = new Array(); //arrary of positions(points) that have to be stored for the path the sections follow
+var hairPath2 = new Array();
+var hairPath3 = new Array();
+var numhairSections = 10; //number of hair sections
+var hairSpacer = 6; //parameter that sets the spacing between sections
+
 
 export default class GameScene extends Phaser.Scene {
-    constructor () {
-        super('Game');
-    }
+	constructor () {
+		super('Game');
+	}
 
 
-    create ()
-    {
+	create ()
+	{
         //  A simple background for our game
         this.add.image(400, 300, 'sky');
 
@@ -42,23 +52,23 @@ export default class GameScene extends Phaser.Scene {
 
         //  Our player animations, turning, walking left and walking right.
         this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
+        	key: 'left',
+        	frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        	frameRate: 10,
+        	repeat: -1
         });
 
         this.anims.create({
-            key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
-            frameRate: 20
+        	key: 'turn',
+        	frames: [ { key: 'dude', frame: 4 } ],
+        	frameRate: 20
         });
 
         this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-            frameRate: 10,
-            repeat: -1
+        	key: 'right',
+        	frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        	frameRate: 10,
+        	repeat: -1
         });
 
         //  Input Events
@@ -66,9 +76,9 @@ export default class GameScene extends Phaser.Scene {
 
         //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
         stars = this.physics.add.group({
-            key: 'star',
-            repeat: 11,
-            setXY: { x: 12, y: 0, stepX: 70 }
+        	key: 'star',
+        	repeat: 2,
+        	setXY: { x: 12, y: 0, stepX: 70 }
         });
 
         stars.children.iterate(function (child) {
@@ -79,6 +89,22 @@ export default class GameScene extends Phaser.Scene {
         });
 
         bombs = this.physics.add.group();
+
+        //  Init hairSection array
+        for (var i = 1; i <= numhairSections-1; i++)
+        {
+        	hairSection1[i] = this.physics.add.sprite(400, 300, 'star');
+        	hairSection2[i] = this.physics.add.sprite(400, 300, 'star');
+        	hairSection3[i] = this.physics.add.sprite(400, 300, 'star');
+        }
+
+    	//  Init hairPath array
+    	for (var i = 0; i <= numhairSections * hairSpacer; i++)
+    	{
+    		hairPath1[i] = new Phaser.Geom.Point(400, 300);
+    		hairPath2[i] = new Phaser.Geom.Point(400, 300);
+    		hairPath3[i] = new Phaser.Geom.Point(400, 300);
+    	}
 
         //  The score
         scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
@@ -91,40 +117,53 @@ export default class GameScene extends Phaser.Scene {
         //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
         this.physics.add.overlap(player, stars, collectStar, null, this);
 
-        this.physics.add.collider(player, bombs, hitBomb, null, this);
+        this.physics.add.collider(hairSection1, bombs, hitBomb, null, this);
     }
 
     update ()
     {
-        if (cursors.left.isDown)
-        {
-            player.setVelocityX(-160);
+    	if (cursors.left.isDown)
+    	{
+    		player.setVelocityX(-160);
+    		updateHair();
 
-            player.anims.play('left', true);
-        }
-        else if (cursors.right.isDown)
-        {
-            player.setVelocityX(160);
+    		player.anims.play('left', true);
+    	}
+    	else if (cursors.right.isDown)
+    	{
+    		player.setVelocityX(160);
+    		updateHair();
 
-            player.anims.play('right', true);
-        }
-        else
-        {
-            player.setVelocityX(0);
+    		player.anims.play('right', true);
+    	}
+    	else if (cursors.up.isDown)
+    	{
+    		player.setVelocityY(-160);
+    		updateHair();
 
-            player.anims.play('turn');
-        }
+    		player.anims.play('right', true);
 
-        if (cursors.up.isDown && player.body.touching.down)
-        {
-            player.setVelocityY(-330);
-        }
+    	}
+    	else if (cursors.down.isDown)
+    	{
+    		player.setVelocityY(160);
+    		updateHair();
+
+    		player.anims.play('right', true);
+    	}
+    	else
+    	{
+    		player.setVelocityX(0);
+    		player.setVelocityY(0);
+
+    		player.anims.play('turn');
+    	}
     }
 };
 
 function collectStar (player, star)
 {
-    star.disableBody(true, true);
+	star.disableBody(true, true);
 
     //  Add and update the score
     score += 10;
@@ -135,7 +174,7 @@ function collectStar (player, star)
         //  A new batch of stars to collect
         stars.children.iterate(function (child) {
 
-            child.enableBody(true, child.x, 0, true, true);
+        	child.enableBody(true, child.x, 0, true, true);
 
         });
 
@@ -150,29 +189,59 @@ function collectStar (player, star)
     }
 }
 
+function updateHair()
+{
+	// Everytime the hair head moves, insert the new location at the start of the array, 
+    // and knock the last position off the end
+
+    var part1 = hairPath1.pop();
+    part1.setTo(player.x - 10, player.y -10);
+    hairPath1.unshift(part1);
+
+    var part2 = hairPath2.pop();
+    part2.setTo(player.x, player.y);
+    hairPath2.unshift(part2);
+
+    var part3 = hairPath3.pop();
+    part3.setTo(player.x +10, player.y +10);
+    hairPath3.unshift(part3);
+
+    for (var i = 1; i <= numhairSections - 1; i++)
+    {
+    	hairSection1[i].x = (hairPath1[i * hairSpacer]).x;
+    	hairSection1[i].y = (hairPath1[i * hairSpacer]).y;
+
+    	hairSection2[i].x = (hairPath2[i * hairSpacer]).x;
+    	hairSection2[i].y = (hairPath2[i * hairSpacer]).y;
+
+    	hairSection3[i].x = (hairPath3[i * hairSpacer]).x;
+    	hairSection3[i].y = (hairPath3[i * hairSpacer]).y;
+    }
+}
+
 function hitBomb (player, bomb)
 {
-    var config = this.game.config;
-    this.model = this.sys.game.globals.model; 
+	var config = this.game.config;
+	this.model = this.sys.game.globals.model; 
 
-    this.physics.pause();
+	this.physics.pause();
 
-    player.setTint(0xff0000);
+	player.setTint(0xff0000);
 
-    player.anims.play('turn');
+	player.anims.play('turn');
 
-    var timer = this.time.delayedCall(1000, function(){
-        var popup = this.add.image(config.width/2, config.height/2, 'deathScene')
-        this.add.text(355, 410, score, { fontSize: '80px', fill: '#FFF' });
-        var menuButton = new Button(this, 200, 550, 'Button', 'ButtonPressed', 'Menu', 'Title');
-        var playButton = new Button(this, 600, 550, 'Button', 'ButtonPressed', 'Play Again', 'Game');
+	var timer = this.time.delayedCall(1000, function(){
+		var popup = this.add.image(config.width/2, config.height/2, 'deathScene')
+		this.add.text(355, 410, score, { fontSize: '80px', fill: '#FFF' });
+		var menuButton = new Button(this, 200, 550, 'Button', 'ButtonPressed', 'Menu', 'Title');
+		var playButton = new Button(this, 600, 550, 'Button', 'ButtonPressed', 'Play Again', 'Game');
 
-        if (score > this.model.highscore) {
-            this.model.highscore = score;
-            var newhighscoretext = this.add.text(500, 290, 'New High Score!', { fontSize: '20px', fill: '#F9BE4F' });
-            newhighscoretext.angle = 35;
-        }
+		if (score > this.model.highscore) {
+			this.model.highscore = score;
+			var newhighscoretext = this.add.text(500, 290, 'New High Score!', { fontSize: '20px', fill: '#F9BE4F' });
+			newhighscoretext.angle = 35;
+		}
 
-        score = 0;
-    }, [], this); 
+		score = 0;
+	}, [], this); 
 }
